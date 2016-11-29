a981ef0ae31153ddbc71eac3fe3dec43cc7c9546
adebimpeadesida <larascores@yahoo.com>
2016-11-29 06:39:54 -0800

    Add files via upload

---
+++ test/minmax.js
@@ -0,0 +1,13 @@
+'use strict';
+  var chai = require('chai');
+  var expect = require('expect');
+
+  var findMinMax = require('../app/library.js').findMinMax;
+
+  describe('Min-Max Numbers in a List: ', function () {
+
+    describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
+
+      it('should return [1,4] for [1, 2, 3 , 4]', function () {
+        expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
+      });
