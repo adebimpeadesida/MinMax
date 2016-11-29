f98beaf31838c2f3c72de3500d662d615e6180cd
adebimpeadesida <larascores@yahoo.com>
2016-11-29 06:43:18 -0800

    Update library.js

--- app/library.js
+++ app/library.js
@@ -1,15 +1,15 @@
 module.exports = {
 findMinMax: function(arr){
-    var maximum = arr[0];
-    var minimum = arr[0];
+    var max = arr[0];
+    var min = arr[0];
     for (var i = 0; i < arr.length; i++){
-        if (arr[i] > maximum){
-            maximum = arr[i];
+        if (arr[i] > max){
+            max = arr[i];
         }
-        if (arr[i] < minimum){
-            minimum = arr[i];
+        if (arr[i] < min){
+            min = arr[i];
         }
     }
-    if (minimum === maximum){
-        return [maximum];
+    if (min === max){
+        return [max];
     }
